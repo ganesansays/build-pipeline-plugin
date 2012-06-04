16e259b0d15c9b0f04dac1a94c74ba4616710d4b
mscata <mscata@hotmail.com>
2012-06-04 14:41:18 +1000

    Clicking on the build number within a build box now takes the user to the build results.

---
+++ src/main/groovy/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildJSONBuilder.groovy
@@ -0,0 +1,48 @@
+package au.com.centrumsystems.hudson.plugin.buildpipeline
+
+import au.com.centrumsystems.hudson.plugin.buildpipeline.PipelineBuild;
+import groovy.json.JsonBuilder
+import hudson.model.Cause;
+import hudson.model.Cause.UserIdCause;
+import hudson.model.Item;
+
+class BuildJSONBuilder {
+
+    static String asJSON(PipelineBuild pipelineBuild, Integer formId, List<Integer> buildDependencyIds) {
+        def builder = new JsonBuilder()
+		def buildStatus = pipelineBuild.currentBuildResult
+        def root = builder {
+			id(formId)			
+            build {				
+				duration(pipelineBuild.buildDuration)				
+				hasPermission(pipelineBuild.project?.hasPermission(Item.BUILD));
+				isBuilding(buildStatus == 'BUILDING')
+				isComplete(buildStatus != 'BUILDING' && buildStatus != 'PENDING' && buildStatus != 'MANUAL')
+				isPending(buildStatus == 'PENDING')
+				isReadyToBeManuallyBuilt(pipelineBuild.isReadyToBeManuallyBuilt())
+				isManualTrigger(pipelineBuild.isManualTrigger())
+				number(pipelineBuild.currentBuild?.number)
+				extId(pipelineBuild.currentBuild?.externalizableId)
+				displayName(pipelineBuild.currentBuild?.displayName)
+				progress(pipelineBuild.buildProgress)
+				progressLeft(100 - pipelineBuild.buildProgress)
+				startDate(pipelineBuild.formattedStartDate)
+				startTime(pipelineBuild.formattedStartTime)
+				status(buildStatus)								
+				url(pipelineBuild.buildResultURL)
+				dependencyIds(buildDependencyIds)
+				hasUpstreamBuild(null != pipelineBuild.upstreamBuild)
+				userId(pipelineBuild.currentBuild?.getCause(Cause.UserIdCause.class)?.getUserId())
+			}
+			project {
+				name(pipelineBuild.project.name)
+				url(pipelineBuild.projectURL)
+			}
+			upstream {
+				projectName(pipelineBuild.upstreamPipelineBuild?.project?.name)
+				buildNumber(pipelineBuild.upstreamBuild?.number)
+			}           
+        }
+        return builder.toString()
+    }
+}
